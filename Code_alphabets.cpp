5b082264687928d24dd6ae815f8e36a01e257605
Updesh arora <updesh.arora4200@gmail.com>
2025-07-29 23:46:01 +0530

    Create Code_alphabets.cpp

---
+++ Code_alphabets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char n = 7;
+    char ch='A';
+    for(char i = 1; i <= n; i++) {
+        for(char j = 1; j <= i; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
